df88694b169fae222e547eeb97acac86a48f3e27
Mikael Marceniuk <mika.marceniuk@gmail.com>
2023-11-27 01:29:12 -0300

    feat: Added dbConn in backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config"
+import "reflect-metadata"
 import Server from "./server"
 ;(async () => {
   const server = new Server()
-  server.init()
+  await server.init()
 })()
--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Express } from "express"
+import { DataSource } from "typeorm"
 
 class Server {
   app: Express
@@ -7,12 +8,30 @@ class Server {
     this.app = express()
   }
 
-  init() {
+  async init() {
+    await this.loadDatabase()
+
     this.app.get("/api", (req, res) => res.json({ msg: "Hello World!" }))
 
     this.app.listen(process.env.PORT, () =>
       console.log("Server is up and running!")
     )
+  }
+
+  private async loadDatabase() {
+    const MongoDbSource = new DataSource({
+      type: "mongodb",
+      host: process.env.MONGODB_HOST,
+      port: Number(process.env.MONGODB_PORT),
+      database: process.env.MONGODB_DB,
+    })
+
+    try {
+      await MongoDbSource.initialize()
+      console.log("Connected to MongoDb")
+    } catch (e) {
+      throw new Error("Could not connect to database")
+    }
   }
 }
 
